d73f50beffc892d9a05f7897ffb9990d3a3f7776
Ankit Barik <barikankitkumar2@gmail.com>
2025-01-05 11:13:15 +0000

    first series

---
+++ first.java
@@ -0,0 +1,20 @@
+public class first{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        // VARIABLES & DATA TYPES
+        // int, float, char, String, boolean
+
+        int a = 3;
+        float b = 3.3f;  // should be use 'f' at the end..
+        char c = 'a';  // should be single quote
+        String d = "admin";  //should be double quote
+        boolean e = true;  
+
+        // should be use '+' for show o/p with strings..
+        System.out.println("integer : " + a);   
+        System.out.println("float : " + b);
+        System.out.println("char : " + c);
+        System.out.println("String : " + d);
+        System.out.println("boolean : " + e);
+    }
+} 
