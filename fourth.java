e8fda841b990cfffa7ceb78ccb4c41d8def4844b
Ankit Barik <barikankitkumar2@gmail.com>
2025-01-06 03:31:17 +0000

    fifth series

---
+++ fourth.java
@@ -0,0 +1,26 @@
+public class fourth {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        // LOOPS
+
+        // while loop
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("while : " + i);
+            i++;
+        }
+
+        // do-while loop
+        int j = 1;
+        do {
+            System.out.println("do-while : " + j);
+            j++;
+        } while (j <= 5);
+
+        // for loop
+        for (int k = 1; k <= 5; k++) {
+            System.out.println("for : " + k);
+        }
+
+    }
+}
