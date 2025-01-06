93e0c234438cb7044b45fc71f288f3ac01470749
Ankit Barik <barikankitkumar2@gmail.com>
2025-01-06 06:20:57 +0000

    eighth series

---
+++ eighth.java
@@ -0,0 +1,30 @@
+public class eighth {
+    int x, y;
+
+    public eighth() {
+        x = 5;
+        System.out.println("Default Constructor");
+    }
+
+    public eighth(int a, int b){
+        x = a;
+        y = b;
+        System.out.println("Parameterized Constructor");
+    }
+
+    public eighth(eighth temp){
+        this.x = temp.x;
+        this.y = temp.y;
+    }
+
+    public static void main(String[] args) {
+        eighth myObj1 = new eighth();
+        System.out.println(myObj1.x);
+
+        eighth myObj2 = new eighth(2,5);
+        System.out.println(myObj2.x + myObj2.y);
+
+        eighth myObj3 = new eighth(myObj2);
+        System.out.println(myObj3);
+    }
+}
