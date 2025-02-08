b8054b68d9afd20d6f060937924e232eccc6e50d
Ankit Barik <barikankitkumar2@gmail.com>
2025-02-08 16:20:51 +0000

    fourteenth series

---
+++ fourteenth.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Student {
+    protected int rollno = 83;
+    String name = "admin";
+
+    void action() {
+        System.out.println("Action : BAD");
+    }
+}
+
+public class fourteenth extends Student {
+    int age = 21;
+
+    public static void main(String[] args) {
+        System.out.println("Inheritance");
+
+        Student s = new Student();
+        System.out.println("Rollno : " + s.rollno);
+        System.out.println("Name : " + s.name);
+        // System.out.println("Age : " + s.age);
+        s.action();
+
+        fourteenth f = new fourteenth();
+        System.out.println("Rollno : " + f.rollno);
+        System.out.println("Name : " + f.name);
+        System.out.println("Age : " + f.age);
+        f.action();
+    }
+}
