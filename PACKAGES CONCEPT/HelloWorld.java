18242fca35009edf9800bd223467fa437114c923
Ankit Barik <barikankitkumar2@gmail.com>
2025-02-08 16:23:07 +0000

    packages series

---
+++ PACKAGES CONCEPT/HelloWorld.java
@@ -0,0 +1,10 @@
+package world;          // create package name
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// for Compile - javac -d . HelloWorld.java
+// for Run - java world.HelloWorld
