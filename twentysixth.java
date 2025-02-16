f5a6da72bb267eddfa7e5075989686a4405e03fa
Ankit Barik <barikankitkumar2@gmail.com>
2025-02-16 02:56:14 +0000

    twentysixth series

--- twentysixth.java
+++ twentysixth.java
@@ -39,7 +39,7 @@ public class twentysixth {
         ch[2][0] = 'N';
         ch[2][1] = 'S';
         for (int i = 0; i < ch.length; i++) {
-            for (int j = 0; j < ch[i].length; j++) {
+            for (int j = 0; j < ch[j].length; j++) {
                 System.out.print(ch[i][j] + " ");
             }
             System.out.println();
